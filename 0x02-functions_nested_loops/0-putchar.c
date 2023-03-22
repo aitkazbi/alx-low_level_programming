e73b5c476281e47934def93d1b40aef4e45c8364
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-21 19:50:40 -0700

    Question0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include 'main.h'
+int main(void)
+{
+-putchar('-');
+-putchar('p');
+-putchar('u');
+-putchar('t');
+-putchar('c');
+-putchar('h');
+-putchar('a');
+-putchar('r');
+return(0);
+}
+		}
