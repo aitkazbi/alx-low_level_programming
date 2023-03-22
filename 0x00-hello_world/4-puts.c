9f233974f090c5e2e67207eab6a741b3cb7b4bf9
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-22 07:45:21 -0700

    Question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main -to print a line with puts function
  */
 int main()
-char ch[] = "Programming is like building a multilingual puzzle";
+char ch[] = "\"Programming is like building a multilingual puzzle";
 puts(ch);
 return 0;
 }
