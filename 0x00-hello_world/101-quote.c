85da395ed9d703f0bf313f87f215ed60de18fc59
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-23 18:29:24 -0700

    Question 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - A C programme that that prints exactly and   * that piece of art is useful"
+  * - Dora Korpar, 2015-10-19,
+  * followed by a new line,  
+  * to the standard error.
+  *return 1
+  */
+int main(void)
+{
+fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-9\n")
+return (1);
+}
