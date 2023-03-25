287b75198d64fea3639c745edca36bedbef5a933
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-25 05:19:09 -0700

    Question 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+  *main return the the alphabet in lowercase
+  *
+  *Return 0
+ */
+int main(void)
+{
+char ch = 'A';
+    putchar(ch); // Écrit le caractère 'A' sur la sortie standard
+    return 0;
+}
