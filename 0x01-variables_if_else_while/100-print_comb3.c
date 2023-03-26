6b55e1d112465ae0083ba111d01bc9ecc2349c71
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 03:40:47 -0700

    Question 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 int n;
 for (n = 0; n <= 89; n++)
 {
-putchar(n);
 if (n <= 9)
 {
-putchar('0');
+putchar(n + '0');
 }
 else
 {
 putchar(n);
+continue;
 }
 putchar(',');
 putchar(' ');
