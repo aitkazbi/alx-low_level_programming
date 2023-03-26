bbf7853b9fc84a98e02103890a69fa7da7a99141
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 00:50:14 -0700

    Question 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main- print the alphabet in lowercase
+  *
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+n++;
+}
+putchar("\n");
+return (0);
+}
