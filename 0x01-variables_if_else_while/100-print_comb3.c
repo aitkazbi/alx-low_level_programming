fe6a7ce43b8cc84ff92b400c65dddce3de52aa12
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 03:33:04 -0700

    Question 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  * main- prints all possible combinations of single-digit numbers.
+  *
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+int n;
+for (n = 0; n <= 89; n++)
+{
+putchar(n);
+if (n <= 9)
+{
+putchar('0');
+}
+else
+{
+putchar(n);
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
