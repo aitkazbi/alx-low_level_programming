73e7dc23661ab9e0f6604fec83179c815349e637
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 01:35:55 -0700

    Question 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main- print the alphabet in lowercase
+  *
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; n <= 102; m++)
+{
+putchar(m);
+}
+printf("\n");
+return (0);
+}
