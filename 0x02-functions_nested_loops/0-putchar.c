f2f014d152f568049b0c808d6251cbb13cd9eaa7
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 21:22:55 -0700

    Question 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,7 @@
   */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar(' ');
+char myChar = '_putchar';
+putchar(myChar);
 return (0);
 }
