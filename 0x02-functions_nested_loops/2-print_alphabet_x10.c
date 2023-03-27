96697c45e76265b46e5ccb2cd236e05180808303
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-27 09:26:47 -0700

    Question 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet(void)
 {
 int i;
 int j;
-for (j = 0; j <= 10; j++)
-}
-for (i = 97; i <= 122; i++)
+for (j = 1; j <= 10; j++)
 {
+for (i = 97; i <= 122; i++)
+{i
 _putchar(i);
 }
 _putchar('\n');
