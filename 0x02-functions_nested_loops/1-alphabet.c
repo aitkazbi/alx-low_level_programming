9ff475d64b82c161fb214e8e450c75cbaea68cac
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-26 21:45:57 -0700

    Question 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main- prints putchar
+  *
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+int i;
+for (i =97; i <= 122; a++)
+{
+_putchar(i);
+}
+_putchar('\n);
+return (0);
+}
