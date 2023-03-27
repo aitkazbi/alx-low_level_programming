1dc93d48a61b3dba80b24065af4deada17067b5f
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-27 07:24:31 -0700

    Question 1

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "print_alphabet"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
