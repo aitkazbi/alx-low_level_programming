b685a57417d7ecf138c11fb299d22309722739be
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-27 09:18:58 -0700

    Question 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Prints the alphabet in lowercase,
+  *followed by a new line.
+  *
+  * Return: Always 0.
+  */
+
+void print_alphabet(void)
+{
+int i;
+int j;
+for (j = 0; j <= 10; j++)
+}
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
