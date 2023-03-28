aea8afdef3814f40f396f44b72a4a7067182fb68
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-28 16:32:26 -0700

    Question 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
 * main - Test function for positive or negative
+*
 * Return: 0
 */
+
 int main(void)
 {
 int i;
@@ -10,4 +14,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-
