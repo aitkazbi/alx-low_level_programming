b20046f8175acb8296754009807681eb8e5bd028
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-30 15:30:47 -0700

    Question 9

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,37 @@
+/**
+ * times_table- prints the 9 times table
+ * Return: No return
+ */
+void times_table(void)
+{
+int a, b, result;
+
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+result = a * b;
+if (b == 0)
+{
+_putchar(result + '0');
+}
+else if (result >= 10)
+{
+_putchar(' ');
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
+}
+if (b != 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
+}
