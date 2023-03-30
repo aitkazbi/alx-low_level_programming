90ead0fdf47a72c6d16e732cb3dabba90f7439cb
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-30 15:51:39 -0700

    Question 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- Adds two integer
+ *@a : variable
+ *@b : variable
+ * Return: Result of a+b
+ */
+int add(int a, int b)
+{
+int som;
+som = a + b;
+return (som);
+}
