18533adbb94c326c28948389d6ab2890ae9bcea5
aitkazbi <khadija.aitkazbi@gmail.com>
2023-03-31 01:14:04 -0700

    Question 3

---
+++ 0x04-more_functions_nested_loops/3-print-numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_numbers- prints the numbers, from 0 to 9.
+  * followed by a new line.
+  *
+  * Return: No return
+  */
+void print_numbers(void)
+{
+ int n;
+ for (n >= 48 && n <= 57)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+
