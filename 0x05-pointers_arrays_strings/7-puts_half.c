72fd594b964772754439eaa6be4658bfe139ce85
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-03 02:23:08 -0700

    Question 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,17 +3,25 @@
 #include <stdio.h>
 
 /**
-  * puts_half -prints half of a string.
-  * @str: Variable
-  *
-  */
+* puts_half -prints half of a string.
+* @str: Variable
+*
+*/
 void puts_half(char *str)
 {
-int i = 0;
-int lent;
-lent = _strlen(&str[i]);
-for (i = 0; i >= lent; i++)
-{
-printf("%s\n", str);
-}
+int indx = 0, len = 0, n;
+
+while (str[indx++])
+len++;
+
+if ((len % 2) == 0)
+n = len / 2;
+
+else
+n = (len + 1) / 2;
+
+for (indx = n; indx < len; indx++)
+_putchar(str[indx]);
+
+_putchar('\n');
 }
