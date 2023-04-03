132e7569d33dd9d8d294dd94c9c25ebc6893203a
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-03 01:20:18 -0700

    Question 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+  * rev_string - reverses a string
+  * @s: Variable
+  * Return: No return
+  */
+
+void rev_string(char *s)
+{
+int i;
+int j = length(s[i])
+for (i = j; i > 0; i--)
+{
+_putchar(s[i]);
+}
+}
