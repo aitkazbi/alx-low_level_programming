9930c495a9a51247406320ceae73a4b99d0b95f1
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-12 07:26:24 -0700

    Question 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,9 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 for (n1 = 0; s1[n1] != '\0'; n1++)
-;
+
 for (n2 = 0; s2[n2] != '\0'; n2++)
-;
+
 stnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
 if (stnew == NULL)
 {
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* **alloc_grid -a pointer to a 2 dimensional array of integers.
+* @width: Variable
+* @height: Variable
+*
+* Return: width or height is 0 or negative, return NULL
+*/
+
+int **alloc_grid(int width, int height)
+{
+int i, j, k, l;
+int **n;
+
+if (width <= 0 || height <= 0)
+return NULL;
+
+n = malloc(sizeof(int *) * height);
+
+if (n == NULL)
+{
+for (i = height - 1; i >= 0; i--)
+{
+free(n[i]);
+}
+free(n);
+return NULL;
+}
+
+for (i = 0; i < height; i++)
+{
+n[i] = malloc(sizeof(int) * width);
+if (n[i] == NULL)
+{
+for (j = i - 1; j >= 0; j--)
+{
+free(n[j]);
+}
+free(n);
+return NULL;
+}
+}
+
+for (k = 0; k < height; k++)
+{
+for (l = 0; l < width; l++)
+{
+n[k][l] = 0;
+}
+}
+
+return (n);
+}
