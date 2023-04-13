16f57c6ed834c75b2ca6456b500905ada78a0ab8
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-13 16:08:05 -0700

    Question 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,19 @@
   */
 int main(int argc, char *argv[])
 {
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
+int num1 = 0;
+int num2 = 0;
 (void) argv;
-if (argc != 3)
+if (argc == 3)
 {
-printf("Error");
-return (1);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", (num1 * num2));
 }
 else
-printf("%d\n", (num1 * num2));
+{
+printf("Error\n");
+return (1);
+}
 return (0);
 }
