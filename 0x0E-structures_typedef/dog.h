80c17b8b8ec46b1542dce3e0e6872513bd648bed
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-18 17:41:04 -0700

    Question 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
   *
   * Description: The attributes of a dog
   */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
