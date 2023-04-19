46fa81ab34d6610e63499aa88e4a13a5c0bcb096
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-19 08:31:20 -0700

    Question 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* array_range- creates an array of integers
+* @min: the addresse of memory to print
+* @max: the size of the memory to print
+*
+* Return: Nothing.
+*/
+int *array_range(int min, int max)
+{
+int *array = NULL;
+int i, t, count;
+
+count = 0;
+if (min > max)
+return (NULL);
+t = (max - min) + 1;
+array = malloc(t * sizeof(int));
+if (array != NULL)
+{
+for (i = min; i <= max; i++)
+{
+array[count] = i;
+count++;
+}
+return (array);
+}
+return (NULL);
+}        
+
