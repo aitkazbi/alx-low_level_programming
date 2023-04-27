77f724870526aa633980515d5687d73785bebc9a
aitkazbi <khadija.aitkazbi@gmail.com>
2023-04-27 16:31:20 -0700

    Question 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - prints before main
+ *
+ * Return: Nothing.
+ *
+ */
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
