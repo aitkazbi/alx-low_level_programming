1cc50dc02f80c491dc1ecc9f108bdcb830a29aab
aitkazbi <khadija.aitkazbi@gmail.com>
2023-05-01 15:13:22 -0700

    Question 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,19 +11,17 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+	listint_t *curr;
 
-listint_t *curr;
-listint_t *temp;
-temp = malloc(sizeof(listint_t));
-
-if (head != NULL)
-{
-curr = *head;
-while ((curr = temp) != NULL)
-{
-curr = curr->next;
-free(temp);
-}
-*head = NULL;
-}
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
