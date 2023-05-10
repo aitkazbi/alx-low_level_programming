96b5671749b2f36c88356473cb222572fc7fda1d
aitkazbi <khadija.aitkazbi@gmail.com>
2023-05-10 08:00:29 -0700

    Creation

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef FIle_IO_H
+#define FILE_IO_H
+#include <stddef.h>
+#include <unistd.h>
+#define STDOUT 1
+
+void _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
