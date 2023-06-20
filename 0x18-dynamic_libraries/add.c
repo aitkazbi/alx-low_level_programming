25ac29706a1ff0975316ab180ed2abb21da2a3dd
aitkazbi <khadija.aitkazbi@gmail.com>
2023-06-20 17:46:53 +0100

    create file tasck 3

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+// Fonction pour l'addition
+int add(int a, int b) 
+{
+    return a + b;
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+// Fonction pour le modulo
+int mod(int a, int b) {
+    return a % b;
+}
